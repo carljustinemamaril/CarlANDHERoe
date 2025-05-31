03f4eaf7e9887d71cfb7831030c76e1e6bcc463f
carljustinemamaril <carlmamaril26@gmail.com>
2025-05-31 21:53:05 +0800

    CARLandHEROE
    CARLandHEROE

---
+++ ClassmatesRPGSimulator_CarlJustineMamaril_Pons/ClassmatesRPGSimulator/Character.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace ClassmatesRPGSimulator
+{
+    public enum CharacterClass
+    {
+        Fighter,
+        Marksman,
+        Assassin
+    }
+
+    public abstract class Character
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int MaxHealth { get; set; }
+        public CharacterClass ClassType { get; protected set; }
+
+        protected static Random rng = new Random();
+
+        public Character(string name, int maxHealth)
+        {
+            Name = name;
+            MaxHealth = maxHealth;
+            Health = maxHealth;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+            if (Health < 0)
+                Health = 0;
+        }
+
+        public abstract int Attack(Character opponent);
+
+        protected bool HasAdvantage(CharacterClass attacker, CharacterClass defender)
+        {
+            return (attacker == CharacterClass.Fighter && defender == CharacterClass.Assassin)
+                || (attacker == CharacterClass.Marksman && defender == CharacterClass.Fighter)
+                || (attacker == CharacterClass.Assassin && defender == CharacterClass.Marksman);
+        }
+    }
+
+
+    public class Carl_Fighter : Character
+    {
+        public Carl_Fighter(string name) : base(name, 110)
+        {
+            ClassType = CharacterClass.Fighter;
+        }
+
+        public override int Attack(Character opponent)
+        {
+            int damage = rng.Next(10, 22);
+            if (HasAdvantage(ClassType, opponent.ClassType))
+                damage = (int)(damage * 1.25);
+            return damage;
+        }
+    }
+
+
+    
+
+    public class Pons_Assassin : Character
+    {
+        public Pons_Assassin(string name) : base(name, 90)
+        {
+            ClassType = CharacterClass.Assassin;
+        }
+
+        public override int Attack(Character opponent)
+        {
+            int damage = rng.Next(14, 24);
+            if (HasAdvantage(ClassType, opponent.ClassType))
+                damage = (int)(damage * 1.25);
+            return damage;
+        }
+    }
+}
